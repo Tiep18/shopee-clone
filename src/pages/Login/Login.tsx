dd6a00ac4fcb1b43ccc24b3608f5100a5ee49050
PHAM VAN TIEP <payvip1802@gmail.com>
2023-06-27 10:57:20 +0700

    UI pages Login/register

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,3 +1,41 @@
+import { Link } from 'react-router-dom'
+
 export default function Login() {
-  return <div>Login</div>
+  return (
+    <div className='bg-orange'>
+      <div className='mx-auto max-w-7xl px-4'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32'>
+          <div className='lg:col-span-2 lg:col-start-4 '>
+            <form className='border-sm flex flex-col justify-center rounded border bg-white px-4 py-8 shadow-md'>
+              <div className=' text-3xl'>Đăng nhập</div>
+              <input
+                type='email'
+                placeholder='Email'
+                className='border-sm outlite-none mt-8 w-full border border-gray-300 p-3 focus:shadow-sm'
+              />
+              <div className='mt-1 min-h-[1rem] text-red-400'>
+                Email không hợp lệ
+              </div>
+              <input
+                type='password'
+                placeholder='Password'
+                className='border-sm outlite-none mt-4 w-full border border-gray-300 p-3 focus:shadow-sm'
+              />
+              <div className='mt-1 min-h-[1rem] text-red-400'></div>
+
+              <button className='mt-4 w-full rounded bg-red-500 p-3 text-white hover:bg-red-700'>
+                Đăng nhập
+              </button>
+              <div className='mt-8 flex items-center justify-center'>
+                <div className='text-gray-400'>Bạn chưa có tài khoản?</div>
+                <Link to={'/register'} className='text-orange ml-1'>
+                  Đăng ký
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/Register/Register.tsx
+++ src/pages/Register/Register.tsx
@@ -1,3 +1,46 @@
+import { Link } from 'react-router-dom'
+
 export default function Register() {
-  return <div>Register</div>
+  return (
+    <div className='bg-orange'>
+      <div className='mx-auto max-w-7xl px-4'>
+        <div className='grid grid-cols-1 py-12 lg:grid-cols-5 lg:py-32'>
+          <div className='lg:col-span-2 lg:col-start-4 '>
+            <form className='border-sm flex flex-col justify-center rounded border bg-white px-4 py-8 shadow-md'>
+              <div className='text-3xl'>Đăng ký</div>
+              <input
+                type='email'
+                placeholder='Email'
+                className='border-sm outlite-none mt-8 w-full border border-gray-300 p-3 focus:shadow-sm'
+              />
+              <div className='mt-1 min-h-[1rem] text-red-400'>
+                Email không hợp lệ
+              </div>
+              <input
+                type='password'
+                placeholder='Password'
+                className='border-sm outlite-none mt-4 w-full border border-gray-300 p-3 focus:shadow-sm'
+              />
+              <div className='mt-1 min-h-[1rem] text-red-400'></div>
+              <input
+                type='password'
+                placeholder='Confirm Password'
+                className='border-sm outlite-none mt-4 w-full border border-gray-300 p-3 focus:shadow-sm'
+              />
+              <div className='mt-1 min-h-[1rem] text-red-400'></div>
+              <button className='mt-4 w-full rounded bg-red-500 p-3 text-white hover:bg-red-700'>
+                Đăng ký
+              </button>
+              <div className='mt-8 flex items-center justify-center'>
+                <div className='text-gray-400'>Bạn đã có tài khoản?</div>
+                <Link to={'/login'} className='text-orange ml-1'>
+                  Đăng nhập
+                </Link>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
